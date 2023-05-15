f30222df0ae6f615c4acd52726b61dcdcd1fb0d9
Homanism <89544626+Homanism@users.noreply.github.com>
2023-05-15 10:29:08 +0200

    fix: connecting all components

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,64 @@
     <v-app>
         <AppBar :is-my-turn="isMyTurn" :my-score="myScore" :opponent-score="opponentScore" />
 
+    
+    <GameCard v-for="(card, index) in myCards" :key="index" :card-data="card" />
+
+    <GameResult v-if="winner" :winner="winner" :is-blackjack="isBlackjack" :myCards="myCards"
+                :opponentCards="opponentCards" :myScore="myScore" :opponentScore="opponentScore"
+                @startNewRound="setNewRound" />
+      
     </v-app>
 </template>
 
 <script>
 import AppBar from './components/AppBar.vue';
-// import GameCard from './components/GameCard.vue';
-// import GameResult from './components/GameResult.vue';
-// import axios from 'axios';
+import GameCard from './components/GameCard.vue';
+import GameResult from './components/GameResult.vue';
+// import shuffleDeckRandomly from './helper/shuffleDeckRandomly.js';
+import axios from 'axios'
 
 export default {
     name: 'App',
 
     components: {
         AppBar,
-        // GameCard,
-        // GameResult
+        GameCard,
+        GameResult
     },
 
+    data: () => ({
+        deckOfCards: [],
+        isMyTurn: true,
+        myScore: 0,
+        opponentScore: 0,
+        myCards: [],
+        opponentCards: [],
+        winner: null,
+        hideOpponentFirstCard: true,
+        drawDialog: false,
+        isBlackjack: false
+    }),
 
+    methods: {
 
+    },
+
+    async mounted() {
+        const response = await axios.get("https://blackjack.ekstern.dev.nav.no/shuffle");
+        console.log(response)
+    },
+
+    computed: {
+
+    },
+
+    watch: {
+
+    }
 };
 </script>
 
 <style>
-
 
 </style>
--- src/components/GameResult.vue
+++ src/components/GameResult.vue
@@ -1,10 +1,17 @@
 <template>
-
+    <div class="container">
+        <h1>RESULT:</h1>
+       
+    </div>
 </template>
 
 <script>
+// import GameCard from './GameCard.vue';
 export default {
-
+    components: {
+        // GameCard
+    },
+    props: ['winner', 'myCards', 'opponentCards', 'myScore', 'opponentScore', 'isBlackjack']
 }
 </script>
 
