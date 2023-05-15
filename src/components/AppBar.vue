c2daae9f2ee58ca5b03a3d9374917bb5934343ac
Homanism <89544626+Homanism@users.noreply.github.com>
2023-05-15 10:50:09 +0200

    fix: Nav logo

--- src/components/AppBar.vue
+++ src/components/AppBar.vue
@@ -1,10 +1,11 @@
 <template>
-    <v-app-bar app color="primary" dark height="80px">
+    <v-app-bar app color="green" dark height="80px">
         <div class="d-flex align-center">
             <v-img alt="Game Logo" class="shrink mr-2" contain src="../assets/blackjack-logo.png"
                 transition="scale-transition" width="40" />
-                <v-img alt="Game Logo" class="shrink mr-2" contain src="https://www.ks.no/imagevault/publishedmedia/ma93si0vhch37c7sars5/Logo_NAV.png"
-                transition="scale-transition" width="40" />
+                <v-img alt="Game Logo" class="shrink mr-2" contain 
+                src="https://www.ks.no/imagevault/publishedmedia/ma93si0vhch37c7sars5/Logo_NAV.png"
+                transition="scale-transition" width="70" />
 
             <h2>Velkommen til Homan BlackJack!</h2>
 
