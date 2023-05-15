98b08a977a2320d98e367d184a89bc9825e6bd7f
Homanism <89544626+Homanism@users.noreply.github.com>
2023-05-15 10:17:18 +0200

    fix: gameCard

--- src/components/GameCard.vue
+++ src/components/GameCard.vue
@@ -1,10 +1,24 @@
 <template>
+    <v-card class="ma-1" width="95" height="136" style="cursor: pointer;">
 
+    </v-card>
 </template>
 
 <script>
 export default {
+    props: {
+        cardData: Object,
+        hideCard: Boolean
+    },
+    data: () => ({
+  
+    }),
+    methods: {
 
+    },
+    computed: {
+
+    }
 }
 </script>
 
